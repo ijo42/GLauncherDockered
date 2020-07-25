d64d46dff430827c890d3a93c95d95a70bf7e488
ijo42 <admin@ijo42.ru>
2020-07-25 23:53:13 +0500

    env names missmatch
    :cringeheralt:

--- samples/build.sh
+++ samples/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -e -p "Enter LaunchServer User Name:" -i "launchserver" NAME
 
-docker image build --build-arg user_id=$(id -u $NAME) --build-arg group_id=$(id -g $NAME) --tag local/launchserver $(pwd)
+docker image build --build-arg UID=$(id -u $NAME) --build-arg GID=$(id -g $NAME) --tag local/launchserver $(pwd)
